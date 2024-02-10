c7d7a03cbec90dc9b8c29850e45bcb962b76a368
Bobko7 <bojip@abv.bg>
2024-02-10 12:16:28 +0200

    Fix link area problem on levels page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function scrollToTopSmoothly() {
-    const scrollDuration = 1700; // 4 seconds
+    const scrollDuration = 1700;
     const startTime = performance.now();
 
     function scroll() {
